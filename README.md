9f522138b8cfa191d4bc28c23a6f03c4b856cbc2
Michael van der Weg <michael@vanderweg.ch>
2014-02-28 13:56:49 +0100

    added tests

--- README.md
+++ README.md
@@ -1,16 +1,58 @@
 # ee-formdata-reader
 
-formdata parser
+simple but powerful formdata parser. parses mutipart & urlencoded data from request. caches files in memory or on the disk.
 
 ## installation
 
 	npm install ee-formdata-reader
 
+
+## build status
+
+[![Build Status](https://travis-ci.org/eventEmitter/ee-formdata-reader.png?branch=master)](https://travis-ci.org/eventEmitter/ee-formdata-reader)
+
+
 ## usage
-
 	
-	var reader = new FormdataReader( request );
+	// reader with defaults
+	var reader = new FormdataReader(request);
 
-	reader.on( "end", function(){
-		log( reader.getForm() );
-	} );
+	// reader with custom limits. all limits are optional
+	var reader = new FormdataReader({
+		  request: request
+		, maxLength: 1000 * 1000 * 10 		// 10mb, defaults to 128mb
+		, maxFormdataLength: 10000 			// 10k for non file form fields, defaults to 2 mb
+		, maxFileLength: 1000 * 1000 * 2 	// 2mb, defaults to 128mb
+		, cachePath: '/tmp' 				// store files on the fs instead in memory, defaults to memory
+		, cacheId: 'some machine id' 		// if files are stored on the filesystem they will contain a 
+											// machine identifier in their filename so one can use shared
+											// storage solutions between mutliple machines. defaults to 
+											// «ee-formdata-reader».
+	});
+
+	// when the request has ended and all data has been received, decoded and cached
+	// the reader will emit an «end» event
+	reader.on('end', function(){
+		// you may retreive the formdata using the «getForm» method
+		log(reader.getForm());
+	});
+
+
+## example
+	
+	var reader = new FormdataReader(request);
+
+	reader.on('end', function(){
+		log(reader.getForm());
+		// name: 'Michael'
+		// files: [
+		//    {
+		//        data: 2e 2e 2e 20 63 6f 6e 74 65 6e 74 73 20 6f 66 20 66 69 6c 65 31 2e 74 78 74 20 2e 2e 2e 
+		//        , filename: "file1.txt"
+		//    }
+		//    , {
+		//        data: 2e 2e 2e 63 6f 6e 74 65 6e 74 73 20 6f 66 20 66 69 6c 65 32 2e 67 69 66 2e 2e 2e 
+		//        , filename: "file2.gif"
+		//    }
+		// ]
+	});
---
+++ test/default.js
@@ -0,0 +1,83 @@
+
+	
+	var   Class 			= require('ee-class')
+		, log 				= require('ee-log')
+		, assert 			= require('assert')
+		, travis 			= require('ee-travis')
+		, fs 				= require('fs')
+		, FormdataReader 	= require('../')
+		, Readable 			= require('stream').Readable
+		, project 			= require('ee-project')
+		, crypto 			= require('crypto');
+
+
+	var md5 = function(buf){
+		return crypto.createHash('md5').update(buf).digest('hex');
+	}
+
+
+	// request simumaltor
+	var createRequest = function(message, contentType){
+		return new (new Class({
+			inherits: Readable
+
+			, init: function(){
+				Readable.call( this );
+				this.file = fs.createReadStream(project.root+'test/'+message);
+
+				this.on('newListener', function(evt, listener){
+					this.file.on(evt, listener);
+				}.bind( this ));
+			}
+
+
+			, getHeader: function(){
+				return contentType;
+			}
+
+
+			, _read: function( size ){}
+		}));
+	}
+	
+
+
+	var calculateMessageHash = function(message) {
+		var hash = '';
+
+		Object.keys(message).forEach(function(key){// log(message[key]);
+			if (Array.isArray(message[key])){
+				message[key].forEach(function(subMessage){
+					hash += md5(subMessage.data ? subMessage.data : subMessage);
+				});
+			}
+			else hash += md5(message[key]);
+		});
+
+		return hash;
+	}
+
+
+
+	describe('The Formdata Reader', function(){
+		it('Should be able to decode post message 1', function(done){
+			var reader = new FormdataReader(createRequest('post1.mime', 'multipart/form-data; boundary=AaB03x'));
+
+			reader.on('end', function(){
+				var form = reader.getForm();
+				assert.equal('4debfdb23bf13d11084b5f6f42f93b897a22164bf25f145183087da4784e81f8cd3b27fe8dee2773db1b7bd3297454cf', calculateMessageHash(form), 'message hash is different!')
+				done();
+			});
+		});
+
+
+		it('Should be able to decode post message 2', function(done){
+			var reader = new FormdataReader(createRequest('post2.mime', 'multipart/mixed; boundary="----------------------------722570873451616639732247"'));
+
+			reader.on('end', function(){
+				var form = reader.getForm();
+				assert.equal('fb93818d01553e9fead6873b780580aec239dddc9c7a19eef9af0052da451a8a', calculateMessageHash(form), 'message hash is different!')
+				done();
+			});
+		});
+	});
